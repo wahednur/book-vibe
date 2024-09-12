16c9241e9eb1637a9a96a3c9b6c2af294aa8228e
Abdul Wahed Nur <wahednur@gmail.com>
2024-09-12 20:16:47 +0600

    Add dumy text in about page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <footer className="px-4 divide-y dark:bg-gray-100 dark:text-gray-800">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
-          <a
+          <Link
             rel="noopener noreferrer"
-            href="#"
+            to="/"
             className="flex justify-center space-x-3 lg:justify-start"
           >
             <div className="flex items-center justify-center w-12 h-12 rounded-full ">
@@ -14,7 +16,7 @@ const Footer = () => {
             <span className="self-center text-2xl font-semibold">
               Book Vibe
             </span>
-          </a>
+          </Link>
         </div>
         <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
           <div className="space-y-3">
